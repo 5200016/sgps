1df529a8c758e74f3c16e9090da6b4b9f47d4add
biyuzhishu <1184090625@qq.com>
2017-07-19 11:16:56 +0800

    master

--- src/app/ZS/fresh-fruit/FreshFruitComponent.ts
+++ src/app/ZS/fresh-fruit/FreshFruitComponent.ts
@@ -10,5 +10,12 @@ import { Component } from '@angular/core';
 })
 
 export class FreshFruitComponent{
-
+  fruitsList = [
+    { img : '../../../assets/specialOffer/fruit1.jpg' , name : '荷兰青啤梨' , price: '39.90'},
+    { img : '../../../assets/specialOffer/fruit2.jpg' , name : '荷兰青啤梨' , price: '39.90'},
+    { img : '../../../assets/specialOffer/fruit3.jpg' , name : '荷兰青啤梨' , price: '39.90'},
+    { img : '../../../assets/specialOffer/fruit1.jpg' , name : '荷兰青啤梨' , price: '39.90'},
+    { img : '../../../assets/specialOffer/fruit2.jpg' , name : '荷兰青啤梨' , price: '39.90'},
+    { img : '../../../assets/specialOffer/fruit3.jpg' , name : '荷兰青啤梨' , price: '39.90'},
+  ]
 }
