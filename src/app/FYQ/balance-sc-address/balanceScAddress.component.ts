1c9a6ab7e26bcf88a3043a7a6dfc59080b41cfbf
biyuzhishu <1184090625@qq.com>
2017-08-13 21:50:59 +0800

    master

---
+++ src/app/FYQ/balance-sc-address/balanceScAddress.component.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by 123 on 2017/7/27.
+ */
+import { Component, Input } from '@angular/core';
+@Component({
+  moduleId: module.id,
+  selector: 'balanceScAddress',
+  styleUrls: ['balanceScAddress.css'],
+  templateUrl: 'balanceScAddress.html',
+  providers: []
+})
+export class BalanceScAddressComponent {
+}
